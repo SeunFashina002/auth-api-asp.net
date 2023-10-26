5f5338ea4e4bf37a9d18260b056b2a575feb442d
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-25 19:18:58 -0700

    added model attribute validations

--- AuthApi/Models/User.cs
+++ AuthApi/Models/User.cs
@@ -5,10 +5,17 @@ namespace AuthApi.Models
     public class User
     {
         [Required(ErrorMessage = "username field is required")]
+        [MinLength(3, ErrorMessage = "username cannot be less than 3 characters")]
+        [StringLength(15, ErrorMessage = "username cannot be more than 15 characters")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "username field is required")]
+        [EmailAddress(ErrorMessage = "enter a valid email address")]
+        public string Email { get; set; }
 
         // *stores only hashed passwords
         [Required(ErrorMessage = "password field is required")]
+        [MinLength(8, ErrorMessage = "password cannot be less than 8 characters")]
         public string Password { get; set; }
     }
 }
