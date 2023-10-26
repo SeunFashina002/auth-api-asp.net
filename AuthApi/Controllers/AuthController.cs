aa295c476c1867fb47bbe1562338a1bb701ce799
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-25 18:44:18 -0700

    added login functionality

--- AuthApi/Controllers/AuthController.cs
+++ AuthApi/Controllers/AuthController.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using AuthApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AuthApi.Controllers
 {
@@ -11,6 +15,13 @@ namespace AuthApi.Controllers
     [Route("api/v1/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public static User user = new();
 
         [HttpPost("register")]
@@ -21,6 +32,43 @@ namespace AuthApi.Controllers
             user.Password = passwordHash;
 
             return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto req)
+        {
+            if (
+                user.Username != req.Username
+                || !BCrypt.Net.BCrypt.Verify(req.Password, user.Password)
+            )
+            {
+                return BadRequest("username or password is incorrect");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim> { new(ClaimTypes.Name, user.Username) };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("jwt:Token").Value!)
+            );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
